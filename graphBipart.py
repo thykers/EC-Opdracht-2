367d3b2c7200cb27c996d155d8414dea717671b0
Maissan,S. (Sylvain) <s.maissan@students.uu.nl>
2021-03-23 16:46:39 +0100

    ils added and what clean up

--- graphBipart.py
+++ graphBipart.py
@@ -4,7 +4,8 @@ import copy
 import cProfile
 import random
 import time
-debug = False
+
+
 class Node:
     index = 0
     gain = 0
@@ -19,7 +20,7 @@ class Node:
 
 
 class NodeDoubleLinkedList(object):
-    global debug
+
     def __init__(self, gain):
         self.head = None
         self.tail = None
@@ -27,8 +28,6 @@ class NodeDoubleLinkedList(object):
         self.gain = gain
 
     def append(self, node):
-        if(debug and node.gain != self.gain):
-            print(f"Adding node with gain {node.gain}",f"to bucket {self.gain}")
         # Append an item 
         if self.head is None:
             self.head = node
@@ -40,13 +39,6 @@ class NodeDoubleLinkedList(object):
             self.tail = node
         node.next = None
         node.is_in_bucket = True
-        if debug:
-            if node.prev != None:
-                if node.prev.gain != node.gain:
-                    print("Wrong append")
-            if node.next != None:
-                if node.next.gain != node.gain:
-                    print("Wrong append")
         self.count += 1
 
     def pop(self):
@@ -65,13 +57,13 @@ class NodeDoubleLinkedList(object):
             print("popping empty list")
 
     def exists(self, node):
-        if(self.head == None):
+        if self.head is None:
             return False
         current = self.head
-        while True and current != None:
-            if(current.index == node.index):
+        while True and current is not None:
+            if current.index == node.index:
                 return True
-            if current.next == None:
+            if current.next is None:
                 print("Node does not exist")
                 return False
             current = current.next
@@ -98,7 +90,7 @@ class NodeDoubleLinkedList(object):
 
 
 class Graph:
-    nodes = []
+    nodes_list = []
     maxLinks = 0
 
     def __init__(self):
@@ -110,181 +102,227 @@ class Graph:
             split = line.split()
             links = list(map(int, split[3:]))
             self.maxLinks = max(len(links), self.maxLinks)
-            self.nodes.append(Node(index + 1, links))
+            self.nodes_list.append(Node(index + 1, links))
 
 
 class Model:
-    global debug
-    fmpass_count = 0
-    fmpass_limit = 500
+    local_search_count_calls = 0
+    max_local_search_calls = 1000
 
     def __init__(self):
         self.Graph = Graph()
         self.maxLinks = self.Graph.maxLinks
-        self.reset()
 
-    def reset(self):
-        self.currentSolution = self.generate_random_solution()
-        self.cuts = self.calculate_cuts()
-
-    def calculate_cuts(self):
+    def calculate_cuts(self, solution):
         cut = 0
-        for node in self.Graph.nodes:
-            own_value = self.currentSolution[node.index - 1]
+        for node in self.Graph.nodes_list:
+            own_value = solution[node.index - 1]
             for link in node.links:
-                if own_value != self.currentSolution[link - 1]:
+                if own_value != solution[link - 1]:
                     cut += 1
         return int(cut / 2)
 
     def generate_random_solution(self):
-        node_amount = len(self.Graph.nodes)
+        node_amount = len(self.Graph.nodes_list)
         solution = np.repeat(0, node_amount)
         solution[int(node_amount / 2):] = np.repeat(1, int(node_amount / 2))
-        #np.random.seed(1)
+        # np.random.seed(1)
         np.random.shuffle(solution)
         return solution
 
     def get_gain(self, i):
-        return [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,-16,-15,-14,-13,-12,-11,-10,-9,-8,-7,-6,-5,-4,-3,-2,-1][i]
+        return \
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, -16, -15, -14, -13, -12, -11, -10, -9, -8, -7,
+             -6, -5, -4, -3, -2, -1][i]
 
     def generate_buckets(self):
         left = [NodeDoubleLinkedList(self.get_gain(i)) for i in range(self.maxLinks * 2 + 1)]
         right = [NodeDoubleLinkedList(self.get_gain(i)) for i in range(self.maxLinks * 2 + 1)]
-        return left, right
+        return [left, right]
 
-    def calculate_gain_node(self, node):
+    def calculate_gain_node(self, solution, node):
         gain = 0
-        own_value = self.currentSolution[node.index - 1]
+        own_value = solution[node.index - 1]
         for link in node.links:
-            if own_value == self.currentSolution[link - 1]:
+            if own_value == solution[link - 1]:
                 gain -= 1
             else:
                 gain += 1
         node.gain = gain
 
-    def calculate_gains_graph(self, left, right):
-        for node in self.Graph.nodes:
-            own_value = self.currentSolution[node.index - 1]
-            self.calculate_gain_node(node)
+    def calculate_gains_graph(self, solution, gain_buckets):
+        for node in self.Graph.nodes_list:
+            own_value = solution[node.index - 1]
+            self.calculate_gain_node(solution, node)
             if own_value == 0:
-                left[node.gain].append(node)
+                gain_buckets[0][node.gain].append(node)
             else:
-                right[node.gain].append(node)
-        return left, right
+                gain_buckets[1][node.gain].append(node)
+        return gain_buckets
 
-    def swap_best(self, use_left, left_bucket, right_bucket):
-        if use_left:
-            current_bucket = left_bucket
-        else:
-            current_bucket = right_bucket
-
+    def swap_best(self, solution, score, bucket_index, gain_buckets):
         for i in range(self.maxLinks, -self.maxLinks - 1, -1):
-            if current_bucket[i].count > 0:
-                selected_node = current_bucket[i].pop()
-                # print(f"Selected Node Index: {selected_node.index}   Gain:{selected_node.gain}")
-                self.currentSolution[selected_node.index - 1] = 1 - self.currentSolution[selected_node.index - 1]
+            if gain_buckets[bucket_index][i].count > 0:
+                selected_node = gain_buckets[bucket_index][i].pop()
+                solution[selected_node.index - 1] = 1 - solution[selected_node.index - 1]
 
                 # Update neighbours
-                for neighbour in [self.Graph.nodes[link - 1] for link in selected_node.links]:
+                for neighbour in [self.Graph.nodes_list[link - 1] for link in selected_node.links]:
                     # Remove from the bucket it is in
                     if neighbour.is_in_bucket:
-                        if self.currentSolution[neighbour.index - 1] == 0:
-                            if debug:
-                                if not (left_bucket[neighbour.gain].exists(neighbour)):
-                                    for i in range(self.maxLinks, -self.maxLinks - 1, -1):
-                                        if left_bucket[i].exists(neighbour):
-                                            print(f"Found neighbour in bucket {i}", f"instead of {neighbour.gain}")
-                            left_bucket[neighbour.gain].remove_node(neighbour)
+                        if solution[neighbour.index - 1] == 0:
+                            gain_buckets[0][neighbour.gain].remove_node(neighbour)
                         else:
-                            if debug:
-                                if not (right_bucket[neighbour.gain].exists(neighbour)):
-                                    for i in range(self.maxLinks, -self.maxLinks - 1, -1):
-                                        if right_bucket[i].exists(neighbour):
-                                            print(f"Found neighbour in bucket {i}", f"instead of {neighbour.gain}")
-                            right_bucket[neighbour.gain].remove_node(neighbour)
+                            gain_buckets[1][neighbour.gain].remove_node(neighbour)
 
                         # Recalculate gain and add to the correct bucket
-                        self.calculate_gain_node(neighbour)
-                        if self.currentSolution[neighbour.index - 1] == 0:
-                            left_bucket[neighbour.gain].append(neighbour)
+                        self.calculate_gain_node(solution, neighbour)
+                        if solution[neighbour.index - 1] == 0:
+                            gain_buckets[0][neighbour.gain].append(neighbour)
                         else:
-                            right_bucket[neighbour.gain].append(neighbour)
-                self.cuts -= selected_node.gain
+                            gain_buckets[1][neighbour.gain].append(neighbour)
+                score -= selected_node.gain
                 break
+        return solution, score
 
-    def is_left_bucket_best(self, left_bucket, right_bucket):
-        for i in range(self.maxLinks, -self.maxLinks - 1, -1):
-            if left_bucket[i].count > 0:
-                return True
-            if right_bucket[i].count > 0:
-                return False
-
-    def print_partition(self):
+    def print_partition(self, solution, score):
         l, r = [], []
-        for i, v in enumerate(self.currentSolution):
+        for i, v in enumerate(solution):
             if v == 0:
                 l.append(i + 1)
             else:
                 r.append(i + 1)
         print(f"left: {l}  |   right: {r}")
-        print(f"Cuts: {self.cuts}")
+        print(f"Cuts: {score}")
 
-    def fm_pass(self):
+    def fm_pass(self, solution):
         # initialize partition and buckets
-        left, right = self.generate_buckets()
-        left_bucket, right_bucket = self.calculate_gains_graph(left, right)
-        use_left = True  ## self.is_left_bucket_best(left_bucket, right_bucket)
-        bestSolution = copy.deepcopy(self.currentSolution)  # Store a copy of the best solution
-        #print(f"Initial: {bestSolution}", f"Cuts: {self.cuts}")
-        #self.print_partition()
-        bestScore = self.cuts
+        score = self.calculate_cuts(solution)
+
+        empty_buckets = self.generate_buckets()
+        gain_buckets = self.calculate_gains_graph(solution, empty_buckets)
+        bucket_index = 0
+
+        best_solution = copy.deepcopy(solution)  # Store a copy of the best solution
+        best_score = score
+        # print(f"Initial: {score}")
 
         # Run
-        for i in range(len(self.Graph.nodes)):
-            #print(f"\nIteration: {i + 1}")
-            # Todo: keep track of best solution.
-            self.swap_best(use_left, left_bucket, right_bucket)
-            if self.cuts < bestScore and i % 2 == 1:        # Only look for better score every two swaps,
-                #print(f"best solution: {bestSolution}")     # because we need an equal amount of nodes in each group.
-                bestSolution = copy.deepcopy(self.currentSolution)
-                bestScore = self.cuts
-            use_left = not use_left
-            #self.print_partition()
+        for i, node in enumerate(self.Graph.nodes_list):
+            solution, score = self.swap_best(solution, score, bucket_index, gain_buckets)
+            if score < best_score and i % 2 == 1:  # Only look for better score every two swaps,
+                # print(f"new best score: {score}")  # because we need an equal amount of nodes in each group.
+                best_solution = copy.deepcopy(solution)
+                best_score = score
+            bucket_index = 1 - bucket_index
 
-        #print(f"\nBest: {bestSolution}  Cuts: {bestScore}")
-        #print(f"Last: {self.currentSolution}  Cuts: {self.cuts}")
-        self.fmpass_count += 1
-        return bestSolution, bestScore
-    
-    def MLS(self):
-        MLS_solution = copy.deepcopy(self.currentSolution)
-        MLS_score = self.cuts
-        while self.fmpass_count < self.fmpass_limit:
-            
+        # print(f"Best score: {best_score}")
+        self.local_search_count_calls += 1
+        return best_solution, best_score
+
+    def mlsk(self):
+        mls_solution = copy.deepcopy(self.currentSolution)
+        mls_score = self.cuts
+        while self.local_search_count_calls < self.max_local_search_calls:
+
             current_run_best = copy.deepcopy(self.currentSolution)
             current_run_best_cuts = self.cuts
             while True:
-                newsolution, newscore = self.fm_pass()
-                if np.array_equal(newsolution, current_run_best):
-                    #print(f"converged with {newscore} cuts")
+                new_solution, new_score = self.fm_pass()
+                if np.array_equal(new_solution, current_run_best):
+                    # TODO: Eventueel een stopping_count bijhouden, dat telt hoe vaak de solutions gelijk zijn. 
+                    # TODO: Zo stopt de Local-search niet als een keer de solutions gelijk zijn. (Zie ils)
+                    # print(f"converged with {new_score} cuts")
                     break
-                if newscore < current_run_best_cuts:
-                    current_run_best = copy.deepcopy(newsolution)
-                    current_run_best_cuts = newscore
-                
-            if current_run_best_cuts < MLS_score:
-                MLS_solution = copy.deepcopy(current_run_best)
-                MLS_score = current_run_best_cuts
+                if new_score < current_run_best_cuts:
+                    current_run_best = copy.deepcopy(new_solution)
+                    current_run_best_cuts = new_score
+
+            if current_run_best_cuts < mls_score:
+                mls_solution = copy.deepcopy(current_run_best)
+                mls_score = current_run_best_cuts
             self.reset()
-        print(self.fmpass_count)
-        print(MLS_solution, MLS_score)
-        return MLS_solution, MLS_score
-            
+        print(self.local_search_count_calls)
+        print(mls_solution, mls_score)
+        return mls_solution, mls_score
+
+    def mls(self, convergence_criteria):
+        solution = self.generate_random_solution()
+        score = self.calculate_cuts(solution)
+
+        mls_solution = copy.deepcopy(solution)
+        mls_score = score
+
+        while self.local_search_count_calls < self.max_local_search_calls:
+            no_change_in_solution_count = 0
+            run_solution = copy.deepcopy(solution)
+            run_score = self.calculate_cuts(run_solution)
+            while no_change_in_solution_count < convergence_criteria:
+                new_solution, new_score = self.fm_pass(solution)
+
+                if new_score >= run_score:
+                    no_change_in_solution_count += 1
+                elif new_score < score:
+                    no_change_in_solution_count = 0
+                    run_solution = copy.deepcopy(new_solution)
+                    run_score = new_score
+
+            if run_score < mls_score:
+                mls_solution = copy.deepcopy(run_solution)
+                mls_score = run_score
+
+            solution = self.generate_random_solution() #mls
+
+        # print(self.local_search_count_calls)
+        print(mls_score)
+        print(f"Cuts verification: {self.calculate_cuts(mls_solution)}")
+        return mls_solution, mls_score
+
+    def mutate_solution(self, solution, perturbation_size):
+        mutated_solution = copy.deepcopy(solution)
+        solution = copy.deepcopy(np.array([1 - bit if (random.uniform(0, 1)) <= perturbation_size else bit
+                                           for bit in mutated_solution]))
+        return solution
+
+    def ils(self, perturbation_size, convergence_criteria):
+        solution = self.generate_random_solution()
+        score = self.calculate_cuts(solution)
+
+        ils_solution = copy.deepcopy(solution)
+        ils_score = score
+
+        while self.local_search_count_calls < self.max_local_search_calls:
+            no_change_in_solution_count = 0
+            run_solution = copy.deepcopy(solution)
+            run_score = self.calculate_cuts(run_solution)
+            while no_change_in_solution_count < convergence_criteria:
+                new_solution, new_score = self.fm_pass(solution)
+
+                if new_score >= run_score:
+                    no_change_in_solution_count += 1
+                elif new_score < score:
+                    no_change_in_solution_count = 0
+                    run_solution = copy.deepcopy(new_solution)
+                    run_score = new_score
+
+            if run_score < ils_score:
+                ils_solution = copy.deepcopy(run_solution)
+                ils_score = run_score
+
+            solution = self.mutate_solution(ils_solution, perturbation_size)
+
+        # print(self.local_search_count_calls)
+        print(ils_score)
+        print(f"Cuts verification: {self.calculate_cuts(ils_solution)}")
+        return ils_solution, ils_score
+
+    def gls(self):
+        ...
 
 
 x = Model()
+
 timer = time.time()
-x.MLS()
+x.mls(5)
+#x.ils(0.05, 5)
 print(f"Elapsed: {time.time() - timer}")
-
-
